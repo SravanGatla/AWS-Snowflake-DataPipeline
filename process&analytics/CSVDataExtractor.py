07c7bf47021aa95c0292fb624ed3173a796affdc
Sravankumar Gatla <90290576+SravanGatla@users.noreply.github.com>
2024-04-15 04:54:01 -0400

    Update CSVDataExtractor.py

--- process&analytics/CSVDataExtractor.py
+++ process&analytics/CSVDataExtractor.py
@@ -2,7 +2,7 @@ from your_module import DataExtractor
 
 class CSVDataExtractor(DataExtractor):
   def extract_data(self):
-    s3 = boto3.client('path of s3')
+    s3 = boto3.client('s3')
     obj = s3.get_object(Bucket = self.bucket_name, key = self.file_key)
     chunks = pd.read_csv(obj['Body'], lines = True, chunksize = self.chunk_size)
     data_chunks = []
