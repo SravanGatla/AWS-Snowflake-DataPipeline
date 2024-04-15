7bd51e9f488597d9a6d6f24032381736da908563
Sravankumar Gatla <90290576+SravanGatla@users.noreply.github.com>
2024-04-15 04:57:32 -0400

    Update JSONDataExtractor.py

--- process&analytics/JSONDataExtractor.py
+++ process&analytics/JSONDataExtractor.py
@@ -2,7 +2,7 @@ from your_module import DataExtractor
 
 class JSONDataExtractor(DataExtractor):
   def extract_data(self):
-    s3 = boto3.client('path of s3')
+    s3 = boto3.client('s3')
     obj = s3.get_object(Bucket = self.bucket_name, file_key = self.file_key)
     chunks = pd.read_json(obj['Body'], lines = True, chunks = self.chunk_size)
     data_chunks = []
