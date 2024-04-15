217f675ddb8830633920edc40a30f143ee64e88b
Sravankumar Gatla <90290576+SravanGatla@users.noreply.github.com>
2024-04-15 05:02:17 -0400

    Update SnowflakeLoader.py

--- process&analytics/SnowflakeLoader.py
+++ process&analytics/SnowflakeLoader.py
@@ -11,14 +11,14 @@ class SnowflakeLoader:
   def load_data_to_snowflake(self):
     try:
       conn = connect(**self.snowflake_connection_params)
-      create_stage_query = f"CREATE OR REPLACE STAGE my_external_stage"
+      create_stage_query = f"CREATE OR REPLACE STAGE XXXXXX_external_stage"
       conn.cursor().execute(create_stage_query)
 
       for i, chunk in enumerate(self.processed_data):
         stage_file_path = f"/data_chunk_{i}.csv"
-        write_pandas(conn, chunk, stage_name = 'my_external_stage', table_name = stage_file_path)
+        write_pandas(conn, chunk, stage_name = 'XXXXXX_external_stage', table_name = stage_file_path)
 
-      copy_into_table_query = f"COPY INTO {self.table_name} FROM '@my_external_stage' FILE_FORMAT = (TYPE = CSV)"
+      copy_into_table_query = f"COPY INTO {self.table_name} FROM '@XXXXXX_external_stage' FILE_FORMAT = (TYPE = CSV)"
       conn.cursor().execute(copy_into_table_query)
       conn.commit()
       print("Data Loaded into Snowflake successfully.")
